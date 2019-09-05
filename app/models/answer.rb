e4ce970cae29981516baab8d51b594e7efd0e1bc
Aleksey Streltsov <a.streltsov@MacBook-Air-Aleksey.local>
2019-09-05 17:33:24 +0300

    Fixed answer position change on update

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,6 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :author, class_name: 'User'
   validates :body, presence: true
+
+  scope :as_appeared, -> { order(created_at: :desc) }
 end
