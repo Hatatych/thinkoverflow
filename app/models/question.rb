9b467ac4b0cfa30e7e0e6043b911ee8674d9bef7
Hatatych <me@hataty.ch>
2019-09-10 13:42:23 +0300

    Best check in models logic fix

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :author, class_name: 'User'
   validates :title, :body, presence: true
+
+  def best_answer
+    answers.best.first
+  end
 end
